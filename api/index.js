5db0e6789dd53bbb91896b62d449cb77c4945104
shake7 <63744052+shakeuy@users.noreply.github.com>
2025-05-11 18:46:32 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://eleccionesdepartamentales2025.corteelectoral.gub.uy/JSON/ResumenGeneral_P_DPTOS.json", {
-    headers: { "User-Agent": "Mozilla/5.0" }
-  });
+  try {
+    const response = await fetch("https://eleccionesdepartamentales2025.corteelectoral.gub.uy/JSON/ResumenGeneral_P_DPTOS.json", {
+      headers: { "User-Agent": "Mozilla/5.0" }
+    });
 
-  const data = await response.text();
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).send(data);
+    if (!response.ok) {
+      return res.status(502).json({ error: "Error al acceder a la Corte Electoral" });
+    }
+
+    const data = await response.text();
+
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
 }
